fa0bab7aa9c279ff2457ad613bb3b3e6110542b2
Winter091 <winter091@yandex.ru>
2018-12-25 00:47:06 +0300

    print and change functions added; Console's code is too messy at the moment

--- Next/Console.py
+++ Next/Console.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import types
 
 from Next.Const import *
 
@@ -23,18 +24,33 @@ class Console(object):
         self.bg = ConsoleBackground()
 
         self.font = pg.font.SysFont('Consolas', 12)
-        self.buffer = [''] * BUFFER_SIZE
+        self.buffer = ['>'] * BUFFER_SIZE
+        self.buffer_index = -1
         self.mode = 0
 
     def update(self, core):
-        if not self.buffer[-1].startswith('>'):
-            self.buffer[-1] = '>'
+
+        self.handle_pressed_keys(core)
+
+    def handle_pressed_keys(self, core):
+        shift = core.keys[304]
+        caps_lock = core.keys[301]
 
         for key in core.pressed_this_frame:
 
-            # Letters
-            if 32 <= key <= 126:
-                self.write_char(key, core)
+            # Usual symbols except dash (45)
+            if 32 <= key <= 44 or 46 <= key <= 126:
+                if shift or caps_lock:
+                    self.print_char(key, upper_case=True)
+                else:
+                    self.print_char(key, upper_case=False)
+
+            # Dash and underscore
+            elif key == 45:
+                if shift:
+                    self.print_char(95, upper_case=False)
+                else:
+                    self.print_char(45, upper_case=False)
 
             # Backspace
             elif key == 8:
@@ -43,20 +59,167 @@ class Console(object):
             # Enter
             elif key == 13:
                 self.goto_next_line()
+                self.process_command(core)
+
+            # PageUp: goto buffer history, up
+            elif key == 280:
+                self.goto_buffer_up()
+
+            # PageDn: goto buffer history, down
+            elif key == 281:
+                self.goto_buffer_down()
 
         # Long pressed keys handling
         for key in range(len(core.keys_duration)):
+
             # key is being pressed for 60 frames or more
             if core.keys_duration[key] >= 60:
-                if 32 <= key <= 126:
-                    self.write_char(key, core)
+
+                # Usual symbols except dash (45)
+                if 32 <= key <= 44 or 46 <= key <= 126:
+                    if shift or caps_lock:
+                        self.print_char(key, upper_case=True)
+                    else:
+                        self.print_char(key, upper_case=False)
+
+                # Dash and underscore
+                elif key == 45:
+                    if shift:
+                        self.print_char(95, upper_case=False)
+                    else:
+                        self.print_char(45, upper_case=False)
+
                 elif key == 8:
                     self.remove_last_char()
 
-    def write_char(self, key, core):
+    def process_command(self, core):
+        command, *args = self.buffer[-2].split(' ')
+
+        if command.startswith('>'):
+            command = command[1:]
+
+        if command == 'help':
+            self.command_help()
+        elif command == 'print':
+            self.command_print(args, core)
+        elif command == 'change':
+            self.command_change(args, core)
+        else:
+            self.print_str('Unknown command: {}'.format(command))
+
+    def command_help(self):
+        self.print_str("You're using S&D MarioPyGame console.")
+        self.print_str("Commands available:")
+        self.print_str('    help - print this message')
+        self.print_str("    print [variable] - print variable's value")
+        self.print_str('    change [variable] [new_value] - change the value of the variable')
+
+    def command_print(self, args, core):
+
+        obj = core
+
+        # Check if the args are correct
+        if len(args):
+            args = args[0].split('.')
+        else:
+            self.print_str('    Usage: print [variable]')
+            return 0
+
+        # Check if the path exists
+        for attr in args:
+            if hasattr(obj, attr):
+                obj = getattr(obj, attr)
+            else:
+                self.print_str("Error: '{}' doesn't have '{}'".format(obj, attr))
+                return 0
+
+        self.print_str(str(obj))
+
+    def command_change(self, args, core):
+
+        obj = core
+
+        # Check if the args are correct
+        if len(args) == 2:
+            path = args[0].split('.')
+            value = args[1]
+        else:
+            self.print_str('    Usage: change [variable] [new_value]')
+            return 0
+
+        # Check if the path exists
+        for attr in path:
+            if hasattr(obj, attr):
+                obj = getattr(obj, attr)
+            else:
+                self.print_str("Error: '{}' doesn't have '{}'".format(obj, attr))
+                return 0
+
+        try:
+            value = int(value)
+        except ValueError:
+            pass
+
+        if len(path) == 2:
+            upper_object = getattr(core, path[0])
+            setattr(upper_object, path[1], value)
+            setattr(core, path[0], upper_object)
+            return 0
+
+        # Get needed object
+        obj = core
+        for attr in path[:-1]:
+            obj = getattr(obj, attr)
+
+        # Change the object's value
+        setattr(obj, path[-1], int(value))
+
+        # Object where we try to find our obj
+        upper_object = core
+
+        # Для имени нашего объекта
+        i = -2
+        # Чтобы спускаться вниз к нашему объекту
+        j = 0
+        while True:
+
+            # Если данная иерархия не имеет нашего объекта
+            if not hasattr(upper_object, path[i]):
+                # Спускаемся на 1 уровень вниз, к нашему объекту
+                # То есть верхним объектом становится объект ниже прошлого
+                upper_object = getattr(upper_object, path[j])
+                j += 1
+
+            # Если мы наконец нашли наш объект
+            else:
+                # Заменяем в upper_object прошлый объект нашим, уже обновлённым
+                setattr(upper_object, path[i], obj)
+
+                # Заменяем наш объект верхним
+                obj = upper_object
+
+                upper_object = core
+                j = 0
+
+                # Наш объект выше, значит его имя тоже в списке path раньше
+                i -= 1
+
+                # Если мы уже дошли до конца, то есть до core
+                if path[i] == path[j]:
+                    break
+
+    def print_str(self, string):
+        self.buffer[-1] = ''
+        for char in string:
+            if char.islower():
+                self.print_char(ord(char), upper_case=False)
+            else:
+                self.print_char(ord(char), upper_case=True)
+        self.goto_next_line()
+
+    def print_char(self, key, upper_case):
         if len(self.buffer[-1]) < 91:
-            # Caps Lock or shift
-            if core.keys[304] or core.keys[301]:
+            if upper_case:
                 self.buffer[-1] += chr(key).upper()
             else:
                 self.buffer[-1] += chr(key)
@@ -65,10 +228,20 @@ class Console(object):
         if len(self.buffer[-1]) > 1:
             self.buffer[-1] = self.buffer[-1][:-1]
 
+    def goto_buffer_up(self):
+        if -self.buffer_index < BUFFER_SIZE:
+            self.buffer_index -= 1
+            self.buffer[-1] = self.buffer[self.buffer_index]
+
+    def goto_buffer_down(self):
+        if self.buffer_index < -1:
+            self.buffer_index += 1
+            self.buffer[-1] = self.buffer[self.buffer_index]
+
     def goto_next_line(self):
+        self.buffer_index = -1
         self.buffer = self.buffer[1:]
-        self.buffer[-1] = self.buffer[-1][1:]
-        self.buffer.append('')
+        self.buffer.append('>'.format(-self.buffer_index))
 
     def change_mode(self):
         if self.mode:
